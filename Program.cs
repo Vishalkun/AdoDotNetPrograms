767bc5576f4f5b08494417b337d7e661293d48fa
Vishal github <Vishal.kunchal@gmail.com>
2023-09-29 20:20:16 +0530

    [vishal kunchal]: Created A Table

--- Program.cs
+++ Program.cs
@@ -5,14 +5,14 @@
         static void Main(string[] args)
         {
            // CustomerOperations.createdatabase();
-            //CustomerOperations.CreateTable();
-            CustomerModel model = new CustomerModel();
+            CustomerOperations.CreateTable();
+            //CustomerModel model = new CustomerModel();
             /*model.Name = "Test";
             model.city = "Mumbai";
             model.phone = 121343;*/
             //CustomerOperations.InsertUsingStoredProcedure(model);
 
-            CustomerOperations.InsertUsingStoredProcedure(model);
+            //CustomerOperations.InsertUsingStoredProcedure(model);
         }
     }
 }
